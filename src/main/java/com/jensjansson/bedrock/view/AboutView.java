f322e6ceaad8793cac25e08071010f812d1f4034
Jens Jansson <peppe@novacode.fi>
2014-02-20 22:46:57 +0200

    Added a list of already added techs into the about view as a reminder to myself.

--- src/main/java/com/jensjansson/bedrock/view/AboutView.java
+++ src/main/java/com/jensjansson/bedrock/view/AboutView.java
@@ -2,13 +2,14 @@ package com.jensjansson.bedrock.view;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
 public class AboutView extends VerticalLayout implements View {
 
 	public AboutView() {
-		Label about = new Label("Welcome!");
+		Label about = new Label("Navigator<br>Logging<br>Authentication<br>", ContentMode.HTML);
 		addComponent(about);
 	}
 	
